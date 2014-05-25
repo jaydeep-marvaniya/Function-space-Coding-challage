1ed0e1eae1d7142350afc2420c10d36746db5f5f
jaydeep <201001132@da-iict.ac.in>
2014-05-25 15:47:02 +0530

    simulotors

--- web.js
+++ web.js
@@ -1,13 +1,20 @@
 var express = require("express");
 var logfmt = require("logfmt");
+var math = require("express");
 var app = express();
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.send('<h1>Hello World!</h1>');
 });
 
+
+app.get('/giveprices', function(req, res) {
+
+
+  res.send('Hello World!');
+});
 
 
 var port = Number(process.env.PORT || 5000);
